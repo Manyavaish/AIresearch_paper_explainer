44a2f8000ff7225082ec47ec20408dc6343aed35
manya vaish <manya.vsh@gmail.com>
2025-11-08 16:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.chains import RetrievalQA
+
 from langchain_groq import ChatGroq
 
 
